98e904a2355d5c952605989fa4369cb811554577
Imtiaz <axubair9@gmail.com>
2024-05-11 15:24:45 +0600

    Set up file

--- First Task/Edge Cheker.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-
-using namespace std;
-
-int main()
-{
-    cout << "Hello brother i am here!";
-    return 0;
-}
---
+++ First Task/Intro.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main()
+{
+    // First Video
+    cout << "Intro of DSA!";
+
+    getch();
+    return 0;
+}
