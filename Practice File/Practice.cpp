2e4dada50c375e4172ea11c6daeb48024d11cb62
Imtiaz <axubair9@gmail.com>
2024-05-18 17:59:12 +0600

    Space complexity practice added

--- Practice File/Practice.cpp
+++ Practice File/Practice.cpp
@@ -94,24 +94,91 @@ int main()
         cause n^4 is bigger than n^2 and n. And 4, 2, 5 is constant thats why we can ignore this
         .
         Example - 1(Nested and double loop):
+        int i, j, n = 3, counter = 0;
+        for(i = 1; i <= n; i++)
+        {
+            for(j = 1; j <= n; j++)
+            {
+                counter = counter + 1;
+            }
+        }
 
-
-    */
-    int i, j, n = 3, counter = 0;
-    for(i = 1; i <= n; i++)
-    {
         for(j = 1; j <= n; j++)
         {
             counter = counter + 1;
         }
-    }
 
-    for(j = 1; j <= n; j++)
+        cout << counter << " Its complexity is O(n^2) + O(n) = O(n^2 + n). Hence n^2 is significantly " <<
+        "bigger than n then its complexity is O(n^2).";
+    */
+
+    /*
+        // Class - 04(Space Complexity):
+        Example - 01:
+        {
+            int n;
+            if(n % 2 == 0)
+            {
+                cout << "Even" << " Its time complexity is O(1) and space complexity is also O(1)";;
+            }
+            else{
+                cout << "Odd" << " Its time complexity is O(1) and space complexity is also O(1)";
+            }
+        }
+        .
+        Example - 02:
+        {
+            int n, i, even[11];
+            for(i = 0; i < 11; i++)
+            {
+                even[i] = 0;
+            }
+
+            for(i = 0; i < 11; i += 2)
+            {
+                even[i] = 1;
+            }
+
+            cin >> n;
+
+            if(even[n])
+            {
+                cout << "Even and space complexity is also O(n) cause its complexity is depend on n";
+            }
+            else{
+                cout << "Odd and space complexity is also O(n) cause its complexity is depend on n";
+            }
+        }
+
+        Example - 03:
+        {
+            int a[3][3] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    }, b[3][3] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    }, s[3][3], i, j, n;
+
+    cin >> n;
+
+    for(i = 0; i < n; i++)
     {
-        counter = counter + 1;
+        for(j = 0; j < n; j++)
+        {
+            s[i][j] = a[i][j] + b[i][j];
+            cout << s[i][j] << " ";
+        }
+        cout << endl;
     }
 
-    cout << counter << " Its complexity is O(n^2) + O(n) = O(n^2 + n). Hence n^2 is significantly " <<
-    "bigger than n then its complexity is O(n^2).";
+    cout << "Complexity is O(n^2)";
+        }
+
+
+    */
+
     return 0;
 }
