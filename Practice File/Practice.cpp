1a4ec9c5b31048913a208c5b4c36d7a0fb3a6d60
Imtiaz <axubair9@gmail.com>
2024-05-26 00:55:59 +0600

    Practice insertion sort done

--- Practice File/Practice.cpp
+++ Practice File/Practice.cpp
@@ -2,30 +2,28 @@
 
 using namespace std;
 
-void bubbleSort(int Arr[], int lengthOfArray)
+void insertionSort(int Arr[], int lengthOfArray)
 {
-    int i ,j , temp;
-    for(i = 0; i < lengthOfArray; i++)
+    int i, j, item;
+    for(i = 1; i < lengthOfArray; i++)
     {
-        for(j = 0; j < lengthOfArray - i - 1; j++)
+        item = Arr[i];
+        j = i - 1;
+        while(j >= 0 && Arr[j] > item )
         {
-            if(Arr[j] > Arr[j + 1])
-            {
-                temp = Arr[j];
-                Arr[j] = Arr[j + 1];
-                Arr[j + 1] = temp;
-            }
+            Arr[j + 1] = Arr[j];
+            j--;
         }
+        Arr[j + 1] = item;
     }
 }
-
 
 int main()
 {
     int arr[] = {8, 7, 6, 5, 4, 6, 2, 4, 6};
     int sizeOfArray = sizeof(arr) / sizeof(arr[0]);
     int target = 91;
-    bubbleSort(arr, sizeOfArray);
+    insertionSort(arr, sizeOfArray);
 
     for(int i = 0; i < sizeOfArray; i++)
     {
